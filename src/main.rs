436005f44831451aa70024a5d92ae3faad6744fb
p6nj <breee@duck.com>
2023-12-13 16:31:43 +0100

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::num::NonZeroUsize;
-
 fn main() {
-    println!("{:?}", primes(NonZeroUsize::new(300).unwrap()));
+    println!("{:?}", divn(1000, 10));
 }
 
-fn primes(max: NonZeroUsize) -> Vec<usize> {
+fn primes(max: usize) -> Vec<usize> {
     (2..((usize::from(max) as f32).sqrt() as usize) + 1).fold(
         (2..=max.into()).collect(),
         |mut acc, n: usize| {
@@ -20,6 +18,28 @@ fn primes(max: NonZeroUsize) -> Vec<usize> {
     )
 }
 
-fn phi(n: NonZeroUsize) -> usize {
-    todo!()
+fn phi(n: usize) -> usize {
+    let prs = primes(n.div_euclid(2));
+    prs.clone()
+        .iter()
+        .cloned()
+        .zip(prs.iter().map(|prime| divn(n, *prime)))
+        .fold(1, |acc, (p, k)| {
+            acc * p.pow((k - 1).try_into().unwrap()) * (p - 1)
+        })
+}
+
+fn divn(n: usize, p: usize) -> usize {
+    match n > p {
+        true => 1 + divn(n.div_euclid(p.into()), p),
+        false => 1,
+    }
+}
+
+fn decomp(n: usize) -> (Vec<usize>, Vec<usize>) {
+    let prs = primes(n.div_euclid(2));
+    (
+        prs.clone(),
+        prs.iter().map(|prime| divn(n, *prime)).collect(),
+    )
 }
