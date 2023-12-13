00192f074e3825a66a8be9f4504a90dc72fc9a72
p6nj <breee@duck.com>
2023-12-13 14:54:53 +0100

    primes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::num::NonZeroUsize;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", primes(NonZeroUsize::new(300).unwrap()));
+}
+
+type N = NonZeroUsize;
+fn primes(max: N) -> Vec<usize> {
+    (2..((usize::from(max) as f32).sqrt() as usize) + 1).fold(
+        (2..=max.into()).collect(),
+        |mut acc, n: usize| {
+            (0..usize::from(max) / n - 1)
+                .map(|i| n.pow(2) + i * n)
+                .for_each(|i| {
+                    if let Ok(index) = acc.binary_search(&i) {
+                        acc.remove(index);
+                    }
+                });
+            acc
+        },
+    )
 }
