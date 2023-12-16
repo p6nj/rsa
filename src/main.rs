4fe497b1a69c6f6bf281e4e0d5a41612e40db19b
p6nj <breee@duck.com>
2023-12-16 22:45:19 +0100

    primes from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,14 @@
 fn main() {
-    println!("{:?}", decomp(13));
+    println!("{:?}", primes(100000));
 }
 
 fn primes(max: usize) -> Vec<usize> {
-    (2..((usize::from(max) as f32).sqrt() as usize) + 1).fold(
-        (2..=max.into()).collect(),
-        |mut acc, n: usize| {
-            (0..usize::from(max) / n - 1)
-                .map(|i| n.pow(2) + i * n)
-                .for_each(|i| {
-                    if let Ok(index) = acc.binary_search(&i) {
-                        acc.remove(index);
-                    }
-                });
-            acc
-        },
-    )
-}
-
-fn phi(n: usize) -> usize {
-    let prs = primes(n.div_euclid(2));
-    prs.clone()
-        .iter()
-        .cloned()
-        .zip(prs.iter().map(|prime| divn(n, *prime)))
-        .fold(1, |acc, (p, k)| {
-            acc * p.pow((k - 1).try_into().unwrap()) * (p - 1)
+    let mut candidates: Vec<Option<usize>> = (2..=max).map(|i| Some(i)).collect();
+    (2..=(max as f32).sqrt() as usize).for_each(|i| {
+        let pow = i.pow(2);
+        (0..=(max - pow).div_euclid(i)).for_each(|j| {
+            candidates[pow + i * j - 2] = None;
         })
-}
-
-fn divn(n: usize, p: usize) -> usize {
-    match n > p {
-        true => divn(n.div_euclid(p.into()), p) + 1,
-        false => 0,
-    }
-}
-
-fn decomp(n: usize) -> (Vec<usize>, Vec<usize>) {
-    let prs = primes(n.div_euclid(2));
-    (
-        prs.clone(),
-        prs.iter()
-            .fold((n, Vec::<usize>::new()), |(acc, mut result), prime| {
-                let n = divn(dbg!(acc), *prime);
-                result.push(n);
-                (dbg!(acc - prime.pow(n.try_into().unwrap())), result)
-            })
-            .1,
-    )
+    });
+    return candidates.iter().cloned().flatten().collect();
 }
