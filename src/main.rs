d31d0108bae18d2aac0e07f48f0835168f56e6e8
p6nj <breee@duck.com>
2023-12-17 19:42:04 +0100

    split tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod tests;
+
 fn main() {
     println!("{:?}", phi(20));
 }
@@ -47,77 +50,4 @@ fn phi(n: usize) -> usize {
         .map(|(p, k)| p.pow((k - 1).try_into().unwrap()) * (p - 1))
         .reduce(|acc, e| acc * e)
         .unwrap()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const HUGE_NUMBER: usize = 10000000;
-
-    #[test]
-    fn primes_normal() {
-        assert_eq!(
-            vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47],
-            primes(50)
-        );
-    }
-
-    #[test]
-    fn primes_illegal() {
-        assert_eq!(Vec::<usize>::new(), primes(0));
-        assert_eq!(Vec::<usize>::new(), primes(1));
-        assert_eq!(vec![2], primes(2));
-    }
-
-    #[test]
-    fn primes_bully() {
-        primes(HUGE_NUMBER);
-    }
-
-    #[test]
-    fn decomp_normal() {
-        assert_eq!(vec![(2, 1), (5, 2)], decomp(50));
-    }
-
-    #[test]
-    fn decomp_prime() {
-        let prime = *primes(500).last().unwrap();
-        assert_eq!(vec![(prime, 1)], decomp(prime));
-    }
-
-    #[test]
-    fn decomp_illegal() {
-        assert_eq!(Vec::<(usize, usize)>::new(), decomp(0));
-        assert_eq!(Vec::<(usize, usize)>::new(), decomp(1));
-        assert_eq!(vec![(2, 1)], decomp(2));
-    }
-
-    #[test]
-    fn decomp_bully() {
-        decomp(HUGE_NUMBER);
-    }
-
-    #[test]
-    fn phi_normal() {
-        assert_eq!(144, phi(456));
-    }
-
-    #[test]
-    fn phi_prime() {
-        let prime = *primes(500).last().unwrap();
-        assert_eq!(prime - 1, phi(prime));
-    }
-
-    #[test]
-    fn phi_illegal() {
-        assert_eq!(0, phi(0));
-        assert_eq!(1, phi(1));
-        assert_eq!(1, phi(2));
-    }
-
-    #[test]
-    fn phi_bully() {
-        phi(HUGE_NUMBER);
-    }
 }
